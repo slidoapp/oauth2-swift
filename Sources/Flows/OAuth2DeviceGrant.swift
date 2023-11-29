a39fbe9bedfa52d720cd5a7727495459632c7f44
Dominik Paľo <dominik@slido.com>
2023-11-29 13:14:41 +0100

    Change device grant copyrights to Cisco Systems

--- Sources/Flows/OAuth2DeviceGrant.swift
+++ Sources/Flows/OAuth2DeviceGrant.swift
@@ -3,7 +3,7 @@
 //  OAuth2
 //
 //  Created by Dominik Paľo on 29/03/23.
-//  Copyright © 2023 Dominik Paľo. All rights reserved.
+//  Copyright © 2023 Cisco Systems, Inc. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
--- Tests/FlowTests/OAuth2DeviceGrantTests.swift
+++ Tests/FlowTests/OAuth2DeviceGrantTests.swift
@@ -3,7 +3,7 @@
 //  OAuth2
 //
 //  Created by Dominik Paľo on 4/12/23.
-//  Copyright 2023 Dominik Paľo
+//  Copyright © 2023 Cisco Systems, Inc. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
